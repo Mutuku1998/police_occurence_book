5bc1ef07afb699b043271f4775c852ace5d98c58
mutuku1998 <martinmutuku1998@gmail.com>
2023-03-06 16:16:35 +0300

    nav bar

---
+++ obproject/src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useRef } from "react";
+import{FaBars,FaTimes} from "react-icons/fa"
+function Navbar(){
+    const navRef= useRef();
+    const showNavbar=() =>{
+        navRef.current.classlist.toggle("responsive_nav");
+    }
+    return(
+      
+<header>
+    <h3>
+    0B registration
+</h3>
+<nav>
+    <a href="#">Home</a>
+     <a href="#">About us</a>
+      <a href="#">Administration</a>
+       <a href="#">Services</a>
+        <a href="#">Contact us</a>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+<FaTimes/>
+        </button>
+</nav>
+<button className="nav-btn" onClick={showNavbar}>
+    <FaBars/>
+</button>
+</header>
+
+    );
+}
+
+export default Navbar;
